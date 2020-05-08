8cedce00e4f5c4564c5a8ebd5c2f7e574b66456a
Taebum Kim <k.taebum@snu.ac.kr>
2020-05-09 01:20:29 +0900

    tflite convert (resnet50)

---
+++ server/scripts/prepare_resnet50.sh
@@ -0,0 +1,23 @@
+# run it in $PROJECT_HOME/server
+
+if [ ! -d "checkpoints" ]
+then
+  mkdir checkpoints
+fi
+
+cd checkpoints
+
+if [ ! -f "resnet50.tar.gz" ]
+then
+  wget https://storage.googleapis.com/cloud-tpu-checkpoints/resnet/resnet50.tar.gz
+fi
+
+if [ ! -d "resnet50" ]
+then
+  tar -xvf resnet50.tar.gz
+
+  mv home/hongkuny/hongkuny_keras_resnet50_gpu_8_fp32_eager_graph_cfit/checkpoints \
+    resnet50
+
+  rm -rf home
+fi
