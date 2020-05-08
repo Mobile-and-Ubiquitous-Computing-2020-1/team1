d655a8a5d46b9b65fe21801069bc0c0d0f2cff04
hs-yn <terry9772@snu.ac.kr>
2020-05-09 01:20:22 +0900

    Basic configuration and interface

--- client/app/src/main/java/com/example/client/MainActivity.java
+++ client/app/src/main/java/com/example/client/MainActivity.java
@@ -4,11 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+
+
 public class MainActivity extends AppCompatActivity {
+    /* Tag id for logging */
+    protected final String tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Load image */
+        ImageView demo_image = (ImageView)findViewById(R.id.demo_image);
+        Bitmap image=((BitmapDrawable)demo_image.getDrawable()).getBitmap();
+
+        /* Load text for displaying result */
+        TextView log_text = (TextView)findViewById(R.id.view_result);
+
+        /* Button for triggering inference*/
+        Button button = (Button)findViewById(R.id.run_inference);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runInference();
+            }
+        });
+    }
+
+    /* Inference */
+    private void runInference() {
+        Log.d(tag, "Inference button triggered");
     }
 }
---
+++ client/app/src/main/java/com/example/client/tflite/Classifier.java
@@ -0,0 +1,7 @@
+package com.example.client.tflite;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+
+public abstract class Classifier {
+}
