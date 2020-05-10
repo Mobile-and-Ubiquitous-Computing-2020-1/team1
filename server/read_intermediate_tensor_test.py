83c20feda5a9b5545305ef753a12b709cb40cdbb
Taebum Kim <k.taebum@snu.ac.kr>
2020-05-11 00:51:56 +0900

    add mobilenet v1 test

--- server/read_intermediate_tensor_test.py
+++ server/read_intermediate_tensor_test.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 def main():
   target_dtype = tf.float32
-  shape_per_tensor = (1, 2048)
+  shape_per_tensor = (28, 28, 256)  # test for mobilenet_v1
   tensor_size = np.prod(shape_per_tensor)
   with open('./intermediate-features/intermediates', 'rb') as f:
     raw_bytes = f.read()
@@ -13,6 +13,7 @@ def main():
     for i in range(0, len(raw_bytes), bytes_size):
       raw_byte = raw_bytes[i:i+bytes_size]
       tensor = tf.io.decode_raw(raw_byte, target_dtype)
+      tensor = tf.reshape(tensor, shape_per_tensor)
       tensors.append(tensor)
 
     tensor = tf.stack(tensors, axis=0)
---
+++ server/scripts/remove_intermediates.sh
@@ -0,0 +1,1 @@
+adb shell "run-as com.example.client rm /data/data/com.example.client/files/intermediates"
